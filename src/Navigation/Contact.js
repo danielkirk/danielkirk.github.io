680fa32b4a7efe4efc96d90a512bbcb5570ab588
imdanielkirk <imdanielkirk@gmail.com>
2019-01-04 15:58:21 -0800

    woooo

---
+++ src/Navigation/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Dance extends Component {
+  render() {
+    return (
+      <footer id="contact" className="contact">
+        <div>
+          <h1 className="offset-md-3 info">...More content is on the way</h1>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Dance;
