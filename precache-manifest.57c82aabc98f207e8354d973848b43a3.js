59599c886ebbe4a85bfa1280e0bd0d885699311e
imdanielkirk <imdanielkirk@gmail.com>
2019-09-07 17:58:48 -0800

    Updates

---
+++ precache-manifest.57c82aabc98f207e8354d973848b43a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4533f195c84acdfb0378",
+    "url": "/static/css/main.68759680.chunk.css"
+  },
+  {
+    "revision": "4533f195c84acdfb0378",
+    "url": "/static/js/main.4533f195.chunk.js"
+  },
+  {
+    "revision": "19020c1f1246b828b769",
+    "url": "/static/css/1.cd84efce.chunk.css"
+  },
+  {
+    "revision": "19020c1f1246b828b769",
+    "url": "/static/js/1.19020c1f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c67056a3a0807f74b9fedcf620ae2d1",
+    "url": "/static/media/me.3c67056a.png"
+  },
+  {
+    "revision": "8fa6f62d8c67e0b49d7b55e761a6aa62",
+    "url": "/static/media/My Post (6).8fa6f62d.jpg"
+  },
+  {
+    "revision": "11b052e1d7e4f5b90b525f709bd1fddc",
+    "url": "/static/media/My Post (7).11b052e1.jpg"
+  },
+  {
+    "revision": "39ddbfda4ddeee04cf98e7577c18cab9",
+    "url": "/static/media/My Post (8).39ddbfda.jpg"
+  },
+  {
+    "revision": "f139d995eeaf597802832c76cfb0eb31",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4f14c49528031d0ace10423e25c79a5.js"
+  "/precache-manifest.57c82aabc98f207e8354d973848b43a3.js"
 );
 
 workbox.clientsClaim();
